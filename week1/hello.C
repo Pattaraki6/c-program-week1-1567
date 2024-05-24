64b9fdfa4ae4e5bf41359267d3ae92c3dec585f1
SUREE7913 <suree7913@gmail.com>
2024-05-24 15:36:34 +0700

    Week1 -cprjgpk

---
+++ week1/hello.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    //Create integer variable
+    int age = 16;
+    //Print Hello to the screen
+    printf("Hello World!\n");
+    printf("My Name is Mr.John Pik!\n");
+    printf("Age: %d\n", age);
+    return 0;
+}
